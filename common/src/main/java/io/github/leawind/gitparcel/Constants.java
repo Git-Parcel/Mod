2a97cc384cd70e8ec7d583ae4fe8f249e872dfc4
Leawind <leawind@yeah.net>
2026-02-21 19:00:17 +0800

    feat(core): update mod id to gitparcel

--- common/src/main/java/io/github/leawind/gitparcel/Constants.java
+++ common/src/main/java/io/github/leawind/gitparcel/Constants.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 
 public class Constants {
 
-  public static final String MOD_ID = "git_parcel";
+  public static final String MOD_ID = "gitparcel";
   public static final String MOD_NAME = "Git Parcel";
   public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 }
