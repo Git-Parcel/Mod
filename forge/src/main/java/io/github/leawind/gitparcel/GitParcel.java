71f4e539783f4fd63a1568b8496c3767afe6e013
Leawind <leawind@yeah.net>
2026-02-21 14:29:06 +0800

    style: reformat

--- forge/src/main/java/io/github/leawind/gitparcel/GitParcel.java
+++ forge/src/main/java/io/github/leawind/gitparcel/GitParcel.java
@@ -5,15 +5,14 @@ import net.minecraftforge.fml.common.Mod;
 @Mod(Constants.MOD_ID)
 public class GitParcel {
 
-    public GitParcel() {
+  public GitParcel() {
 
-        // This method is invoked by the Forge mod loader when it is ready
-        // to load your mod. You can access Forge and Common code in this
-        // project.
+    // This method is invoked by the Forge mod loader when it is ready
+    // to load your mod. You can access Forge and Common code in this
+    // project.
 
-        // Use Forge to bootstrap the Common mod.
-        Constants.LOG.info("Hello Forge world!");
-        CommonClass.init();
-
-    }
+    // Use Forge to bootstrap the Common mod.
+    Constants.LOG.info("Hello Forge world!");
+    CommonClass.init();
+  }
 }
