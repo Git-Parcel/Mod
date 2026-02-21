b590c7beb674d560dc69217bbce9472d57e0aa2e
Leawind <leawind@yeah.net>
2026-02-21 17:26:31 +0800

    refactor: simplify platform helper interface and initialization logging

--- neoforge/src/main/java/io/github/leawind/gitparcel/platform/NeoForgePlatformHelper.java
+++ neoforge/src/main/java/io/github/leawind/gitparcel/platform/NeoForgePlatformHelper.java
@@ -7,18 +7,6 @@ import net.neoforged.fml.loading.FMLLoader;
 public class NeoForgePlatformHelper implements IPlatformHelper {
 
   @Override
-  public String getPlatformName() {
-
-    return "NeoForge";
-  }
-
-  @Override
-  public boolean isModLoaded(String modId) {
-
-    return ModList.get().isLoaded(modId);
-  }
-
-  @Override
   public boolean isDevelopmentEnvironment() {
 
     return !FMLLoader.getCurrent().isProduction();
