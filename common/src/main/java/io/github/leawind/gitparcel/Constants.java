199ab17c54a2449a9f4cbe7b8b54bfce14e3fb19
Leawind <leawind@yeah.net>
2026-02-21 14:07:29 +0800

    chore: rename

---
+++ common/src/main/java/io/github/leawind/gitparcel/Constants.java
@@ -0,0 +1,11 @@
+package io.github.leawind.gitparcel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "git_parcel";
+	public static final String MOD_NAME = "Git Parcel";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
