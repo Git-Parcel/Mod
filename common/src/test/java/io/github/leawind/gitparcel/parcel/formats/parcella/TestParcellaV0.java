16b397981da3a9660fbf518e6cc105e1c5f016c0
Leawind <leawind@yeah.net>
2026-02-24 14:50:51 +0800

    test(common): add JUnit testing and initial test case

---
+++ common/src/test/java/io/github/leawind/gitparcel/parcel/formats/parcella/TestParcellaV0.java
@@ -0,0 +1,10 @@
+package io.github.leawind.gitparcel.parcel.formats.parcella;
+
+import org.junit.jupiter.api.Test;
+
+public class TestParcellaV0 {
+  @Test
+  void test() {
+    System.out.println("Hello test!");
+  }
+}
