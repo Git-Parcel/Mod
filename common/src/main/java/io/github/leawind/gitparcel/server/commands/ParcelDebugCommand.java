9aa16594548e45665a0a1769ef6a677b3fe4cbdb
Leawind <leawind@yeah.net>
2026-02-26 08:56:23 +0800

    style(commands): restructure ParcelDebugCommand with helper methods

--- common/src/main/java/io/github/leawind/gitparcel/server/commands/ParcelDebugCommand.java
+++ common/src/main/java/io/github/leawind/gitparcel/server/commands/ParcelDebugCommand.java
@@ -1,6 +1,8 @@
 package io.github.leawind.gitparcel.server.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import io.github.leawind.gitparcel.Constants;
 import io.github.leawind.gitparcel.parcel.ParcelFormat;
 import io.github.leawind.gitparcel.server.commands.arguments.FilePathArgument;
@@ -17,7 +19,8 @@ public class ParcelDebugCommand {
 
   public static void register(
       CommandDispatcher<CommandSourceStack> dispatcher, CommandBuildContext context) {
-    final var commandSave =
+
+    var commandSave =
         Commands.literal("save")
             .then(
                 Commands.argument("from", BlockPosArgument.blockPos())
@@ -25,40 +28,19 @@ public class ParcelDebugCommand {
                         Commands.argument("to", BlockPosArgument.blockPos())
                             .then(
                                 Commands.argument("path", FilePathArgument.filePath())
-                                    .executes(
-                                        ctx ->
-                                            saveParcel(
-                                                ctx.getSource(),
-                                                BlockPosArgument.getLoadedBlockPos(ctx, "from"),
-                                                BlockPosArgument.getLoadedBlockPos(ctx, "to"),
-                                                FilePathArgument.getPath(ctx, "path"),
-                                                Constants.PARCEL_FORMATS.defaultSaver()))
+                                    .executes(ParcelDebugCommand::saveDefaultFormat)
                                     .then(
                                         Commands.argument("format", ParcelFormatArgument.saver())
-                                            .executes(
-                                                ctx ->
-                                                    saveParcel(
-                                                        ctx.getSource(),
-                                                        BlockPosArgument.getLoadedBlockPos(
-                                                            ctx, "from"),
-                                                        BlockPosArgument.getLoadedBlockPos(
-                                                            ctx, "to"),
-                                                        FilePathArgument.getPath(ctx, "path"),
-                                                        ParcelFormatArgument.getSaver(
-                                                            ctx, "format")))))));
-    final var commandLoad =
+                                            .executes(ParcelDebugCommand::save)))));
+
+    var commandLoad =
         Commands.literal("load")
             .then(
                 Commands.argument("from", BlockPosArgument.blockPos())
                     .then(
                         Commands.argument("path", FilePathArgument.filePath())
-                            .executes(
-                                ctx ->
-                                    loadParcel(
-                                        ctx.getSource(),
-                                        BlockPosArgument.getLoadedBlockPos(ctx, "from"),
-                                        FilePathArgument.getPath(ctx, "path")))));
-    final var command =
+                            .executes(ParcelDebugCommand::load)));
+    var command =
         Commands.literal("parcel_debug")
             .requires(Commands.hasPermission(Commands.LEVEL_GAMEMASTERS))
             .then(commandSave)
@@ -67,7 +49,26 @@ public class ParcelDebugCommand {
     dispatcher.register(command);
   }
 
-  public static int saveParcel(
+  private static int saveDefaultFormat(CommandContext<CommandSourceStack> ctx)
+      throws CommandSyntaxException {
+    return save(
+        ctx.getSource(),
+        BlockPosArgument.getLoadedBlockPos(ctx, "from"),
+        BlockPosArgument.getLoadedBlockPos(ctx, "to"),
+        FilePathArgument.getPath(ctx, "path"),
+        Constants.PARCEL_FORMATS.defaultSaver());
+  }
+
+  private static int save(CommandContext<CommandSourceStack> ctx) throws CommandSyntaxException {
+    return save(
+        ctx.getSource(),
+        BlockPosArgument.getLoadedBlockPos(ctx, "from"),
+        BlockPosArgument.getLoadedBlockPos(ctx, "to"),
+        FilePathArgument.getPath(ctx, "path"),
+        ParcelFormatArgument.getSaver(ctx, "format"));
+  }
+
+  public static int save(
       CommandSourceStack source, BlockPos from, Vec3i to, Path path, ParcelFormat.Save format) {
     try {
       format.save(source.getLevel(), from, to, path, true, true);
@@ -80,7 +81,14 @@ public class ParcelDebugCommand {
     }
   }
 
-  public static int loadParcel(CommandSourceStack source, BlockPos origin, Path path) {
+  private static int load(CommandContext<CommandSourceStack> ctx) throws CommandSyntaxException {
+    return load(
+        ctx.getSource(),
+        BlockPosArgument.getLoadedBlockPos(ctx, "from"),
+        FilePathArgument.getPath(ctx, "path"));
+  }
+
+  public static int load(CommandSourceStack source, BlockPos origin, Path path) {
     Constants.LOG.info("Loading parcel {} from {}", origin, path);
     return 0;
   }
