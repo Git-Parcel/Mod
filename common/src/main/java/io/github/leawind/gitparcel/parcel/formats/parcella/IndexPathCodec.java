fc7fa18f3ea1084f89a91ad8c36ff5b17be0d8fd
Leawind <leawind@yeah.net>
2026-02-27 00:23:40 +0800

    refactor(parcel): optimize index to path conversion

--- common/src/main/java/io/github/leawind/gitparcel/parcel/formats/parcella/IndexPathCodec.java
+++ common/src/main/java/io/github/leawind/gitparcel/parcel/formats/parcella/IndexPathCodec.java
@@ -1,31 +1,59 @@
 package io.github.leawind.gitparcel.parcel.formats.parcella;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public final class IndexPathCodec {
-
   static Path indexToPath(Path root, long index, String suffix) {
+    return root.resolve(indexToPath(index, suffix));
+  }
+
+  static Path indexToPath(long index, String suffix) {
+    var parts = indexToPathParts(index);
+    return Path.of(String.join(File.separator, parts) + suffix);
+  }
+
+  static List<String> indexToPathParts(long index) {
+    if (index < CACHE_SIZE) {
+      return CACHE.get((int) index);
+    }
+    return indexToPathPartsImpl(index);
+  }
+
+  static List<String> indexToPathPartsImpl(long index) {
     if (index == 0) {
-      return root.resolve("00" + suffix);
+      return List.of("00");
     }
 
-    Path result = root;
-    long value = index;
+    var parts = new ArrayList<String>(32);
+    do {
+      parts.add(toHex((int) (index & 0xFF)));
+      index >>>= 8;
+    } while (index != 0);
+    return parts;
+  }
 
-    List<String> parts = new ArrayList<>();
-    while (value != 0) {
-      int b = (int) (value & 0xFF);
-      parts.add(String.format("%02X", b));
-      value >>>= 8;
+  static String toHex(int value) {
+    return HEX_DIGITS[value];
+  }
+
+  static final String[] HEX_DIGITS = new String[0x100];
+
+  static {
+    for (int i = 0; i < 256; i++) {
+      HEX_DIGITS[i] = String.format("%02X", i);
     }
+  }
 
-    int last = parts.size() - 1;
-    for (int i = 0; i < last; i++) {
-      result = result.resolve(parts.get(i));
-    }
+  private static final int CACHE_SIZE = 512;
+  private static final ArrayList<List<String>> CACHE = new ArrayList<>(CACHE_SIZE);
 
-    return result.resolve(parts.get(last) + suffix);
+  static {
+    IntStream.range(0, CACHE_SIZE)
+        .mapToObj(IndexPathCodec::indexToPathPartsImpl)
+        .forEach(CACHE::add);
   }
 }
--- common/src/test/java/io/github/leawind/gitparcel/parcel/formats/parcella/IndexPathCodecTest.java
+++ common/src/test/java/io/github/leawind/gitparcel/parcel/formats/parcella/IndexPathCodecTest.java
@@ -1,18 +1,33 @@
 package io.github.leawind.gitparcel.parcel.formats.parcella;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.nio.file.Path;
+import java.util.List;
 import java.util.function.BiConsumer;
 import org.junit.jupiter.api.Test;
 
 public class IndexPathCodecTest {
 
   @Test
-  void testIndexToPath() {
+  void testIndexToPathParts() {
+    assertEquals(List.of("00"), IndexPathCodec.indexToPathParts(0x00L));
+    assertEquals(List.of("01"), IndexPathCodec.indexToPathParts(0x01L));
+    assertEquals(List.of("FF"), IndexPathCodec.indexToPathParts(0xFFL));
+    assertEquals(List.of("34", "12"), IndexPathCodec.indexToPathParts(0x1234L));
+    assertEquals(List.of("12", "34", "56"), IndexPathCodec.indexToPathParts(0x563412L));
+    assertEquals(List.of("12", "34", "06"), IndexPathCodec.indexToPathParts(0x63412L));
+    assertEquals(List.of("12", "34", "06", "FF"), IndexPathCodec.indexToPathParts(0xFF063412L));
+    assertEquals(List.of("FF", "FF", "FF", "FF"), IndexPathCodec.indexToPathParts(0xFFFFFFFFL));
+  }
+
+  @Test
+  void testIndexToPath2() {
     var cwd = Path.of(".");
     BiConsumer<Long, String> test =
-        (index, path) -> {
-          assert IndexPathCodec.indexToPath(cwd, index, ".dat").equals(cwd.resolve(path));
-        };
+        (index, path) ->
+            assertEquals(IndexPathCodec.indexToPath(cwd, index, ".dat"), cwd.resolve(path));
+
     test.accept(0x1234L, "34/12.dat");
 
     test.accept(0x00L, "00.dat");
@@ -25,5 +40,6 @@ public class IndexPathCodecTest {
 
     test.accept(0x01240FL, "0F/24/01.dat");
     test.accept(0x31240FL, "0F/24/31.dat");
+    test.accept(0x010203040506L, "06/05/04/03/02/01.dat");
   }
 }
