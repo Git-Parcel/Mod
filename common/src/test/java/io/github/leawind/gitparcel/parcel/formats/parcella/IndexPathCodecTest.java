00d1406ae3337db0cc2ec6ad26552629a6266730
Leawind <leawind@yeah.net>
2026-02-27 08:58:23 +0800

    test(parcel): add test cases for IndexPathCodecTest

--- common/src/test/java/io/github/leawind/gitparcel/parcel/formats/parcella/IndexPathCodecTest.java
+++ common/src/test/java/io/github/leawind/gitparcel/parcel/formats/parcella/IndexPathCodecTest.java
@@ -30,14 +30,18 @@ public class IndexPathCodecTest {
 
     test.accept(0x00L, "00.dat");
     test.accept(0x01L, "01.dat");
+    test.accept(0x06L, "06.dat");
+    test.accept(0x0FL, "0F.dat");
     test.accept(0xFFL, "FF.dat");
 
-    test.accept(0x0FL, "0F.dat");
-
+    test.accept(0x0100L, "00/01.dat");
+    test.accept(0x0703L, "03/07.dat");
     test.accept(0x240FL, "0F/24.dat");
+    test.accept(0xFF00L, "00/FF.dat");
 
     test.accept(0x01240FL, "0F/24/01.dat");
     test.accept(0x31240FL, "0F/24/31.dat");
+
     test.accept(0x010203040506L, "06/05/04/03/02/01.dat");
   }
 }
