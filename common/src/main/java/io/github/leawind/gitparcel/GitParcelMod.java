30be49fbafa50e789709331dd23ed7bae2674713
Leawind <leawind@yeah.net>
2026-03-02 07:35:11 +0800

    refactor(parcel): Extract format registration into separate method

--- common/src/main/java/io/github/leawind/gitparcel/GitParcelMod.java
+++ common/src/main/java/io/github/leawind/gitparcel/GitParcelMod.java
@@ -1,6 +1,5 @@
 package io.github.leawind.gitparcel;
 
-import io.github.leawind.gitparcel.parcel.ParcelFormat;
 import io.github.leawind.gitparcel.parcel.formats.mvp.MvpFormatV0;
 import io.github.leawind.gitparcel.parcel.formats.parcella.ParcellaD16FormatV0;
 import io.github.leawind.gitparcel.parcel.formats.parcella.ParcellaD32FormatV0;
@@ -10,14 +9,20 @@ import io.github.leawind.gitparcel.platform.Services;
 public class GitParcelMod {
 
   public static void init() {
-    Constants.PARCEL_FORMATS
-        .register(new ParcellaD16FormatV0.Save())
-        .registerDefault(new ParcellaD32FormatV0.Save())
+    registerFormats();
+  }
+
+  private static void registerFormats() {
+    var formatManager = Constants.PARCEL_FORMATS;
+
+    formatManager.register(new ParcellaD16FormatV0.Save());
+    formatManager.registerDefault(new ParcellaD32FormatV0.Save());
+    formatManager
         .register(new StructureTemplateFormatV0.Save())
         .register(new StructureTemplateFormatV0.Load());
 
     if (Services.PLATFORM.isDevelopmentEnvironment()) {
-      Constants.PARCEL_FORMATS.register(new MvpFormatV0.Save());
+      formatManager.register(new MvpFormatV0.Save());
     }
   }
 }
