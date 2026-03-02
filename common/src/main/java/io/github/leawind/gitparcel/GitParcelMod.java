deb1ffa19a3efecb386eadb5f84fd6d806d302b4
Leawind <leawind@yeah.net>
2026-03-02 18:04:52 +0800

    refactor(common): move ParcellaD16FormatV0 registration to development environment

--- common/src/main/java/io/github/leawind/gitparcel/GitParcelMod.java
+++ common/src/main/java/io/github/leawind/gitparcel/GitParcelMod.java
@@ -15,13 +15,13 @@ public class GitParcelMod {
   private static void registerFormats() {
     var formatManager = Constants.PARCEL_FORMATS;
 
-    formatManager.register(new ParcellaD16FormatV0.Save());
     formatManager.registerDefault(new ParcellaD32FormatV0.Save());
     formatManager
         .register(new StructureTemplateFormatV0.Save())
         .register(new StructureTemplateFormatV0.Load());
 
     if (Services.PLATFORM.isDevelopmentEnvironment()) {
+      formatManager.register(new ParcellaD16FormatV0.Save());
       formatManager.register(new MvpFormatV0.Save());
     }
   }
