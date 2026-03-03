ece7bd856f3dfd750f4f747708b083321bb230e2
Leawind <leawind@yeah.net>
2026-03-03 10:39:22 +0800

    refactor(fabric): rename server mod class to dedicated server

--- fabric/src/main/java/io/github/leawind/gitparcel/GitParcelModFabricDedicatedServer.java
+++ fabric/src/main/java/io/github/leawind/gitparcel/GitParcelModFabricDedicatedServer.java
@@ -2,7 +2,7 @@ package io.github.leawind.gitparcel;
 
 import net.fabricmc.api.DedicatedServerModInitializer;
 
-public class GitParcelModFabricServer implements DedicatedServerModInitializer {
+public class GitParcelModFabricDedicatedServer implements DedicatedServerModInitializer {
   @Override
   public void onInitializeServer() {
     GitParcelModDedicatedServer.init();
