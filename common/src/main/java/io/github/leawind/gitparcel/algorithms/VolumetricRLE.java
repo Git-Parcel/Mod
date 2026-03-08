5381674bde5ef023c4821d3fb5a8ddc049044ea0
Leawind <leawind@yeah.net>
2026-03-08 15:42:11 +0800

    refactor(algo): simplify VolumetricRLE

--- common/src/main/java/io/github/leawind/gitparcel/algorithms/VolumetricRLE.java
+++ common/src/main/java/io/github/leawind/gitparcel/algorithms/VolumetricRLE.java
@@ -42,104 +42,95 @@ public interface VolumetricRLE {
     int get(int x, int y, int z);
   }
 
-  interface ResultFactory<T> {
-    T create(int value, int originX, int originY, int originZ, int sizeX, int sizeY, int sizeZ);
-  }
-
   VolumetricRLE.Encoder IMPL =
-      new VolumetricRLE.Encoder() {
+      (sizeX, sizeY, sizeZ, values) -> {
+        final int sizeXZ = sizeX * sizeZ;
+        final int totalSize = sizeY * sizeXZ;
 
-        @Override
-        public List<Run> encode(int sizeX, int sizeY, int sizeZ, ValueGetter values) {
+        ArrayList<Run> result = new ArrayList<>(totalSize / 2 + 1);
 
-          final int sizeXZ = sizeX * sizeZ;
-          final int totalSize = sizeY * sizeXZ;
-
-          ArrayList<Run> result = new ArrayList<>(totalSize / 2 + 1);
-
-          final int[] valueGrid = new int[totalSize];
-          final boolean[] visited = new boolean[totalSize];
-          for (int y = 0; y < sizeY; y++) {
-            final int yOffset = y * sizeXZ;
-            for (int x = 0; x < sizeX; x++) {
-              final int xOffset = yOffset + x * sizeZ;
-              for (int z = 0; z < sizeZ; z++) {
-                // idx = xOffset + z;
-                valueGrid[xOffset + z] = values.get(x, y, z);
-              }
+        final int[] valueGrid = new int[totalSize];
+        final boolean[] visited = new boolean[totalSize];
+        for (int y = 0; y < sizeY; y++) {
+          final int yOffset = y * sizeXZ;
+          for (int x = 0; x < sizeX; x++) {
+            final int xOffset = yOffset + x * sizeZ;
+            for (int z = 0; z < sizeZ; z++) {
+              // idx = xOffset + z;
+              valueGrid[xOffset + z] = values.get(x, y, z);
             }
           }
+        }
 
-          int[] groupIndices = new int[totalSize];
-          int groupIndicesSize = 0;
+        int[] groupIndices = new int[totalSize];
+        int groupIndicesSize;
 
-          for (int y = 0; y < sizeY; y++) {
-            final int yOffset = y * sizeXZ;
+        for (int y = 0; y < sizeY; y++) {
+          final int yOffset = y * sizeXZ;
 
-            for (int x = 0; x < sizeX; x++) {
-              final int xSizeZ = x * sizeZ;
-              final int xOffset = yOffset + xSizeZ;
+          for (int x = 0; x < sizeX; x++) {
+            final int xSizeZ = x * sizeZ;
+            final int xOffset = yOffset + xSizeZ;
 
-              for (int z = 0; z < sizeZ; z++) {
-                final int idx = xOffset + z;
+            for (int z = 0; z < sizeZ; z++) {
+              final int idx = xOffset + z;
 
-                if (visited[idx]) continue;
-                groupIndicesSize = 0;
+              if (visited[idx]) continue;
+              groupIndicesSize = 0;
 
-                final int value = valueGrid[idx];
+              final int value = valueGrid[idx];
 
-                int boundY = y + 1;
-                int boundX = x + 1;
-                int boundZ = z + 1;
+              int boundY = y + 1;
+              int boundX = x + 1;
+              int boundZ = z + 1;
 
-                // Y
-                while (boundY < sizeY) {
-                  final int tryIdx = boundY * sizeXZ + xSizeZ + z;
-                  if (visited[tryIdx] || valueGrid[tryIdx] != value) break;
-                  visited[tryIdx] = true;
+              // Y
+              while (boundY < sizeY) {
+                final int tryIdx = boundY * sizeXZ + xSizeZ + z;
+                if (visited[tryIdx] || valueGrid[tryIdx] != value) break;
+                visited[tryIdx] = true;
 
-                  groupIndices[groupIndicesSize++] = tryIdx;
-                  boundY++;
+                groupIndices[groupIndicesSize++] = tryIdx;
+                boundY++;
+              }
+
+              // X
+              extendX:
+              while (boundX < sizeX) {
+                int i = 0;
+                for (int tryY = y; tryY < boundY; tryY++) {
+                  final int tryIdx = tryY * sizeXZ + boundX * sizeZ + z;
+                  if (visited[tryIdx] || valueGrid[tryIdx] != value) break extendX;
+                  groupIndices[groupIndicesSize + i++] = tryIdx;
                 }
+                groupIndicesSize += i;
+                boundX++;
+              }
 
-                // X
-                extendX:
-                while (boundX < sizeX) {
-                  int i = 0;
-                  for (int tryY = y; tryY < boundY; tryY++) {
-                    final int tryIdx = tryY * sizeXZ + boundX * sizeZ + z;
-                    if (visited[tryIdx] || valueGrid[tryIdx] != value) break extendX;
+              // Z
+              extendZ:
+              while (boundZ < sizeZ) {
+                int i = 0;
+                for (int tryY = y; tryY < boundY; tryY++) {
+                  for (int tryX = x; tryX < boundX; tryX++) {
+                    final int tryIdx = tryY * sizeXZ + tryX * sizeZ + boundZ;
+                    if (visited[tryIdx] || valueGrid[tryIdx] != value) break extendZ;
                     groupIndices[groupIndicesSize + i++] = tryIdx;
                   }
-                  groupIndicesSize += i;
-                  boundX++;
                 }
-
-                // Z
-                extendZ:
-                while (boundZ < sizeZ) {
-                  int i = 0;
-                  for (int tryY = y; tryY < boundY; tryY++) {
-                    for (int tryX = x; tryX < boundX; tryX++) {
-                      final int tryIdx = tryY * sizeXZ + tryX * sizeZ + boundZ;
-                      if (visited[tryIdx] || valueGrid[tryIdx] != value) break extendZ;
-                      groupIndices[groupIndicesSize + i++] = tryIdx;
-                    }
-                  }
-                  groupIndicesSize += i;
-                  boundZ++;
-                }
-
-                // Mark as visited
-                for (int i = 0; i < groupIndicesSize; i++) {
-                  visited[groupIndices[i]] = true;
-                }
-                result.add(new Run(value, x, y, z, boundX, boundY, boundZ));
+                groupIndicesSize += i;
+                boundZ++;
               }
+
+              // Mark as visited
+              for (int i = 0; i < groupIndicesSize; i++) {
+                visited[groupIndices[i]] = true;
+              }
+              result.add(new Run(value, x, y, z, boundX, boundY, boundZ));
             }
           }
-
-          return result;
         }
+
+        return result;
       };
 }
