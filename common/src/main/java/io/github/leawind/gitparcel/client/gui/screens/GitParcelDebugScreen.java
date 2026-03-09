3095cf9b3055946127255bd70cbc8f1b8e43bf8d
Leawind <leawind@yeah.net>
2026-03-09 19:11:36 +0800

    feat(gui): add unused debug screen

---
+++ common/src/main/java/io/github/leawind/gitparcel/client/gui/screens/GitParcelDebugScreen.java
@@ -0,0 +1,52 @@
+package io.github.leawind.gitparcel.client.gui.screens;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.toasts.SystemToast;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import org.jspecify.annotations.NonNull;
+
+public class GitParcelDebugScreen extends Screen {
+  protected GitParcelDebugScreen(Component title) {
+    super(title);
+  }
+
+  @Override
+  protected void init() {
+    Button buttonWidget =
+        Button.builder(
+                Component.literal("Hello World"),
+                (btn) -> {
+                  // When the button is clicked, we can display a toast to the screen.
+                  minecraft
+                      .getToastManager()
+                      .addToast(
+                          SystemToast.multiline(
+                              minecraft,
+                              SystemToast.SystemToastId.NARRATOR_TOGGLE,
+                              Component.nullToEmpty("Hello World!"),
+                              Component.nullToEmpty("This is a toast.")));
+                })
+            .bounds(40, 40, 120, 20)
+            .build();
+    // x, y, width, height
+    // It's recommended to use the fixed height of 20 to prevent rendering issues with the button
+    // textures.
+
+    // Register the button widget.
+    addRenderableWidget(buttonWidget);
+  }
+
+  @Override
+  public void render(@NonNull GuiGraphics graphics, int mouseX, int mouseY, float delta) {
+    super.render(graphics, mouseX, mouseY, delta);
+
+    // Minecraft doesn't have a "label" widget, so we'll have to draw our own text.
+    // We'll subtract the font height from the Y position to make the text appear above the button.
+    // Subtracting an extra 10 pixels will give the text some padding.
+    // textRenderer, text, x, y, color, hasShadow
+    graphics.drawString(
+        this.font, "Special Button", 40, 40 - this.font.lineHeight - 10, 0xFFFFFFFF, true);
+  }
+}
