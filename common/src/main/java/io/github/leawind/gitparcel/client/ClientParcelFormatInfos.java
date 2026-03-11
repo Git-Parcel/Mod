678b3b06a5e9f65926be1b99eaf582a22d1605f1
Leawind <leawind@yeah.net>
2026-03-11 08:47:37 +0800

    feat(client): add cache field for parcel format infos

--- common/src/main/java/io/github/leawind/gitparcel/client/ClientParcelFormatInfos.java
+++ common/src/main/java/io/github/leawind/gitparcel/client/ClientParcelFormatInfos.java
@@ -4,6 +4,7 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import io.github.leawind.gitparcel.api.parcel.ParcelFormat;
 import java.util.List;
+import org.jspecify.annotations.Nullable;
 
 public record ClientParcelFormatInfos(
     List<ParcelFormat.Info> savers, List<ParcelFormat.Info> loaders) {
@@ -20,4 +21,13 @@ public record ClientParcelFormatInfos(
                           .fieldOf("loaders")
                           .forGetter(ClientParcelFormatInfos::loaders))
                   .apply(inst, ClientParcelFormatInfos::new));
+
+  /**
+   * Cache of the parcel format infos received from the server.
+   *
+   * <p>Updated when received {@code UpdateParcelFormatInfosS2CPacket}.
+   *
+   * <p>Should be cleared when the client disconnects from the server.
+   */
+  public static @Nullable ClientParcelFormatInfos CACHE = null;
 }
