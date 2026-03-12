58e51591713ab324d8d0fccd9230851483717b79
Leawind <leawind@yeah.net>
2026-03-12 13:14:08 +0800

    refactor(commands): split parcel debug command

---
+++ common/src/main/java/io/github/leawind/gitparcel/server/commands/parcel_debug/ParcelDebugLoadSubcommand.java
@@ -0,0 +1,102 @@
+package io.github.leawind.gitparcel.server.commands.parcel_debug;
+
+import com.mojang.brigadier.builder.ArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import io.github.leawind.gitparcel.GitParcelTranslations;
+import io.github.leawind.gitparcel.api.parcel.ParcelFormat;
+import io.github.leawind.gitparcel.api.parcel.ParcelTransform;
+import io.github.leawind.gitparcel.api.parcel.exceptions.ParcelException;
+import io.github.leawind.gitparcel.server.commands.arguments.DirPathArgument;
+import java.io.IOException;
+import java.nio.file.Path;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.TemplateMirrorArgument;
+import net.minecraft.commands.arguments.TemplateRotationArgument;
+import net.minecraft.commands.arguments.coordinates.BlockPosArgument;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.Rotation;
+
+public class ParcelDebugLoadSubcommand {
+  public static ArgumentBuilder<CommandSourceStack, ?> build() {
+
+    var load_rotation =
+        Commands.argument("rotation", TemplateRotationArgument.templateRotation())
+            .executes(ParcelDebugLoadSubcommand::load3);
+
+    var load_mirror =
+        Commands.argument("mirror", TemplateMirrorArgument.templateMirror())
+            .executes(ParcelDebugLoadSubcommand::load2)
+            .then(load_rotation);
+
+    var load_path =
+        Commands.argument("path", DirPathArgument.path())
+            .executes(ParcelDebugLoadSubcommand::load1)
+            .then(load_mirror);
+
+    var load_from = Commands.argument("from", BlockPosArgument.blockPos()).then(load_path);
+
+    return Commands.literal("load").then(load_from);
+  }
+
+  private static int load1(CommandContext<CommandSourceStack> ctx) throws CommandSyntaxException {
+    return load(
+        ctx.getSource(),
+        BlockPosArgument.getLoadedBlockPos(ctx, "from"),
+        DirPathArgument.getPath(ctx, "path"),
+        Mirror.NONE,
+        Rotation.NONE);
+  }
+
+  private static int load2(CommandContext<CommandSourceStack> ctx) throws CommandSyntaxException {
+    return load(
+        ctx.getSource(),
+        BlockPosArgument.getLoadedBlockPos(ctx, "from"),
+        DirPathArgument.getPath(ctx, "path"),
+        TemplateMirrorArgument.getMirror(ctx, "mirror"),
+        Rotation.NONE);
+  }
+
+  private static int load3(CommandContext<CommandSourceStack> ctx) throws CommandSyntaxException {
+    return load(
+        ctx.getSource(),
+        BlockPosArgument.getLoadedBlockPos(ctx, "from"),
+        DirPathArgument.getPath(ctx, "path"),
+        TemplateMirrorArgument.getMirror(ctx, "mirror"),
+        TemplateRotationArgument.getRotation(ctx, "rotation"));
+  }
+
+  private static int load(
+      CommandSourceStack source, BlockPos pos, Path path, Mirror mirror, Rotation rotation) {
+    return load(source, new ParcelTransform(mirror, rotation, pos), path);
+  }
+
+  private static int load(CommandSourceStack source, ParcelTransform transform, Path path) {
+    final int loadFlags =
+        Block.UPDATE_CLIENTS
+            | Block.UPDATE_IMMEDIATE
+            | Block.UPDATE_KNOWN_SHAPE
+            | Block.UPDATE_SKIP_ALL_SIDEEFFECTS;
+
+    try {
+      // TODO load entities
+      ParcelFormat.load(source.getLevel(), transform, path, false, true, loadFlags);
+
+      source.sendSuccess(() -> GitParcelTranslations.of("command.parcel_debug.load.success"), true);
+      return 1;
+    } catch (IOException | ParcelException e) {
+      ParcelDebugCommand.LOGGER.error("Error while loading parcel", e);
+      source.sendFailure(
+          GitParcelTranslations.of("command.parcel_debug.load.failure", e.getMessage()));
+      return 0;
+    } catch (Exception e) {
+      ParcelDebugCommand.LOGGER.error("Unexpected error while loading parcel", e);
+      source.sendFailure(
+          GitParcelTranslations.of("command.parcel_debug.unexpected_error", e.getMessage()));
+      return 0;
+    }
+  }
+}
---
+++ common/src/main/java/io/github/leawind/gitparcel/server/commands/parcel_debug/ParcelDebugSaveSubcommand.java
@@ -0,0 +1,160 @@
+package io.github.leawind.gitparcel.server.commands.parcel_debug;
+
+import com.mojang.brigadier.arguments.BoolArgumentType;
+import com.mojang.brigadier.builder.ArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import io.github.leawind.gitparcel.GitParcelTranslations;
+import io.github.leawind.gitparcel.api.parcel.ParcelFormat;
+import io.github.leawind.gitparcel.api.parcel.ParcelFormatRegistry;
+import io.github.leawind.gitparcel.api.parcel.ParcelMeta;
+import io.github.leawind.gitparcel.api.parcel.ParcelTransform;
+import io.github.leawind.gitparcel.api.parcel.exceptions.ParcelException;
+import io.github.leawind.gitparcel.server.commands.arguments.DirPathArgument;
+import io.github.leawind.gitparcel.server.commands.arguments.ParcelFormatArgument;
+import java.io.IOException;
+import java.nio.file.Path;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.TemplateMirrorArgument;
+import net.minecraft.commands.arguments.TemplateRotationArgument;
+import net.minecraft.commands.arguments.coordinates.BlockPosArgument;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Vec3i;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.levelgen.structure.BoundingBox;
+
+public class ParcelDebugSaveSubcommand {
+  public static ArgumentBuilder<CommandSourceStack, ?> build() {
+
+    var save_rotation =
+        Commands.argument("rotation", TemplateRotationArgument.templateRotation())
+            .executes(ParcelDebugSaveSubcommand::save5);
+
+    var save_mirror =
+        Commands.argument("mirror", TemplateMirrorArgument.templateMirror())
+            .executes(ParcelDebugSaveSubcommand::save4)
+            .then(save_rotation);
+
+    var save_ignore_entities =
+        Commands.argument("ignore_entities", BoolArgumentType.bool())
+            .executes(ParcelDebugSaveSubcommand::save3)
+            .then(save_mirror);
+
+    var save_format =
+        Commands.argument("format", ParcelFormatArgument.saver())
+            .executes(ParcelDebugSaveSubcommand::save2)
+            .then(save_ignore_entities);
+
+    var save_path =
+        Commands.argument("path", DirPathArgument.path())
+            .executes(ParcelDebugSaveSubcommand::save1)
+            .then(save_format);
+
+    var save_to = Commands.argument("to", BlockPosArgument.blockPos()).then(save_path);
+
+    var save_from = Commands.argument("from", BlockPosArgument.blockPos()).then(save_to);
+
+    return Commands.literal("save").then(save_from);
+  }
+
+  private static int save1(CommandContext<CommandSourceStack> ctx) throws CommandSyntaxException {
+    return save(
+        ctx.getSource(),
+        BlockPosArgument.getLoadedBlockPos(ctx, "from"),
+        BlockPosArgument.getLoadedBlockPos(ctx, "to"),
+        DirPathArgument.getPath(ctx, "path"),
+        ParcelFormatRegistry.INSTANCE.defaultSaver(),
+        true,
+        Mirror.NONE,
+        Rotation.NONE);
+  }
+
+  private static int save2(CommandContext<CommandSourceStack> ctx) throws CommandSyntaxException {
+    return save(
+        ctx.getSource(),
+        BlockPosArgument.getLoadedBlockPos(ctx, "from"),
+        BlockPosArgument.getLoadedBlockPos(ctx, "to"),
+        DirPathArgument.getPath(ctx, "path"),
+        ParcelFormatArgument.getSaver(ctx, "format"),
+        true,
+        Mirror.NONE,
+        Rotation.NONE);
+  }
+
+  private static int save3(CommandContext<CommandSourceStack> ctx) throws CommandSyntaxException {
+    return save(
+        ctx.getSource(),
+        BlockPosArgument.getLoadedBlockPos(ctx, "from"),
+        BlockPosArgument.getLoadedBlockPos(ctx, "to"),
+        DirPathArgument.getPath(ctx, "path"),
+        ParcelFormatArgument.getSaver(ctx, "format"),
+        BoolArgumentType.getBool(ctx, "ignore_entities"),
+        Mirror.NONE,
+        Rotation.NONE);
+  }
+
+  private static int save4(CommandContext<CommandSourceStack> ctx) throws CommandSyntaxException {
+    return save(
+        ctx.getSource(),
+        BlockPosArgument.getLoadedBlockPos(ctx, "from"),
+        BlockPosArgument.getLoadedBlockPos(ctx, "to"),
+        DirPathArgument.getPath(ctx, "path"),
+        ParcelFormatArgument.getSaver(ctx, "format"),
+        BoolArgumentType.getBool(ctx, "ignore_entities"),
+        TemplateMirrorArgument.getMirror(ctx, "mirror"),
+        Rotation.NONE);
+  }
+
+  private static int save5(CommandContext<CommandSourceStack> ctx) throws CommandSyntaxException {
+    return save(
+        ctx.getSource(),
+        BlockPosArgument.getLoadedBlockPos(ctx, "from"),
+        BlockPosArgument.getLoadedBlockPos(ctx, "to"),
+        DirPathArgument.getPath(ctx, "path"),
+        ParcelFormatArgument.getSaver(ctx, "format"),
+        BoolArgumentType.getBool(ctx, "ignore_entities"),
+        TemplateMirrorArgument.getMirror(ctx, "mirror"),
+        TemplateRotationArgument.getRotation(ctx, "rotation"));
+  }
+
+  private static int save(
+      CommandSourceStack source,
+      BlockPos corner1,
+      BlockPos corner2,
+      Path parcelDir,
+      ParcelFormat.Save<?> format,
+      boolean ignoreEntities,
+      Mirror mirror,
+      Rotation rotation) {
+    try {
+      BoundingBox boundingBox = BoundingBox.fromCorners(corner1, corner2);
+      ParcelTransform transform = new ParcelTransform(mirror, rotation, boundingBox);
+
+      Vec3i sizeWorldSpace =
+          new Vec3i(boundingBox.getXSpan(), boundingBox.getYSpan(), boundingBox.getZSpan());
+      Vec3i sizeParcelSpace = ParcelTransform.rotateSizeInverted(rotation, sizeWorldSpace);
+
+      ParcelMeta meta = ParcelMeta.create(format.id(), format.version(), sizeParcelSpace);
+
+      ParcelFormat.save(source.getLevel(), transform, meta, parcelDir, ignoreEntities);
+
+      source.sendSuccess(
+          () -> GitParcelTranslations.of("command.parcel_debug.save.success"), ignoreEntities);
+
+      return 1;
+
+    } catch (IOException | ParcelException e) {
+      ParcelDebugCommand.LOGGER.error("Error while saving parcel", e);
+      source.sendFailure(
+          GitParcelTranslations.of("command.parcel_debug.save.failure", e.getMessage()));
+      return 0;
+    } catch (Exception e) {
+      ParcelDebugCommand.LOGGER.error("Unexpected error while saving parcel", e);
+      source.sendFailure(
+          GitParcelTranslations.of("command.parcel_debug.unexpected_error", e.getMessage()));
+      return 0;
+    }
+  }
+}
