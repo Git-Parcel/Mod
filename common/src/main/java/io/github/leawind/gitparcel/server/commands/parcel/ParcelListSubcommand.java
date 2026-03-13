c4c586e9131e0d33412c8d8a59a1db75aa15968b
Leawind <leawind@yeah.net>
2026-03-13 08:14:29 +0800

    refactor(commands): remove command /parcel list instance

--- common/src/main/java/io/github/leawind/gitparcel/server/commands/parcel/ParcelListSubcommand.java
+++ common/src/main/java/io/github/leawind/gitparcel/server/commands/parcel/ParcelListSubcommand.java
@@ -6,8 +6,6 @@ import io.github.leawind.gitparcel.api.parcel.ParcelFormat;
 import io.github.leawind.gitparcel.api.parcel.ParcelFormatRegistry;
 import io.github.leawind.gitparcel.permission.WorldPermissions;
 import io.github.leawind.gitparcel.server.commands.GitParcelBaseCommand;
-import io.github.leawind.gitparcel.world.gitparcel.GitParcelLevelSavedData;
-import io.github.leawind.gitparcel.world.gitparcel.ParcelInstance;
 import java.util.List;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
@@ -66,44 +64,6 @@ public class ParcelListSubcommand extends GitParcelBaseCommand {
                   return 1;
                 });
 
-    // /parcel list parcel_instance
-    var parcel_list_parcel_instance =
-        Commands.literal("parcel_instance")
-            .executes(
-                (ctx) -> {
-                  var source = ctx.getSource();
-
-                  if (!validateWorldPermission(source, WorldPermissions.LIST_INSTANCE)) {
-                    return 0;
-                  }
-
-                  var level = source.getLevel();
-                  var savedData = GitParcelLevelSavedData.get(level);
-                  List<ParcelInstance> instances = savedData.listParcelInstances();
-
-                  source.sendSuccess(
-                      () ->
-                          GitParcelTranslations.of(
-                              "command.parcel.list.parcel_instance.header", instances.size()),
-                      false);
-
-                  for (var instance : instances) {
-                    source.sendSuccess(
-                        () ->
-                            Component.literal("  - UUID: ")
-                                .append(Component.literal(instance.uuid().toString()))
-                                .append(Component.literal(", Box: "))
-                                .append(Component.literal(instance.boundingBox().toString()))
-                                .append(Component.literal(", Rotation: "))
-                                .append(Component.literal(instance.rotation().name()))
-                                .append(Component.literal(", Mirror: "))
-                                .append(Component.literal(instance.mirror().name())),
-                        false);
-                  }
-
-                  return 1;
-                });
-
-    return Commands.literal("list").then(parcel_list_format).then(parcel_list_parcel_instance);
+    return Commands.literal("list").then(parcel_list_format);
   }
 }
