f11d95492cb26f305c8fe347f7c05c76183447b5
Leawind <leawind@yeah.net>
2026-04-25 21:11:46 +0800

    refactor(gameTest): simplify test iteration with utility method
    - Extract rotation/mirror combination logic into reusable utility
    - Streamline test method to focus on assertion behavior
    - Reduce code duplication across format feature combinations

--- common/src/main/java/io/github/leawind/gitparcel/gametest/GitParcelGameTest.java
+++ common/src/main/java/io/github/leawind/gitparcel/gametest/GitParcelGameTest.java
@@ -2,36 +2,19 @@ package io.github.leawind.gitparcel.gametest;
 
 import com.mojang.logging.LogUtils;
 import io.github.leawind.gitparcel.GitParcel;
-import io.github.leawind.gitparcel.api.parcel.ParcelFormat;
 import io.github.leawind.gitparcel.api.parcel.ParcelFormatRegistry;
-import io.github.leawind.gitparcel.api.parcel.exceptions.ParcelException;
 import io.github.leawind.gitparcel.gametest.utils.GameTestHelpMore;
+import io.github.leawind.gitparcel.gametest.utils.GitParcelTestUtils;
 import io.github.leawind.inventory.misc.TempDirectory;
-import java.io.IOException;
-import net.minecraft.world.level.block.Mirror;
-import net.minecraft.world.level.block.Rotation;
 import org.slf4j.Logger;
 
 public class GitParcelGameTest {
   public static final Logger LOGGER = LogUtils.getLogger();
 
-  public void testSavers(GameTestHelpMore helper) throws IOException, ParcelException {
-    ParcelFormat.Saver<?>[] savers =
-        ParcelFormatRegistry.INSTANCE.streamSavers().toArray(ParcelFormat.Saver[]::new);
-
-    for (var format : savers) {
-      Rotation[] rotations =
-          format.features().contains(ParcelFormat.Feature.ROTATE)
-              ? Rotation.values()
-              : new Rotation[] {Rotation.NONE};
-
-      Mirror[] mirrors =
-          format.features().contains(ParcelFormat.Feature.MIRROR)
-              ? Mirror.values()
-              : new Mirror[] {Mirror.NONE};
-
-      for (Rotation rotation : rotations) {
-        for (Mirror mirror : mirrors) {
+  public void testSavers(GameTestHelpMore helper) throws Exception {
+    GitParcelTestUtils.forEachFormatCombination(
+        ParcelFormatRegistry.INSTANCE.streamSavers().toList(),
+        (format, rotation, mirror) -> {
           try (var tempDir = new TempDirectory(GitParcel.MOD_ID)) {
             LOGGER.info(
                 "Testing format {} with rotation={} mirror={}", format.spec(), rotation, mirror);
@@ -45,12 +28,9 @@ public class GitParcelGameTest {
                 tempDir.getPath(),
                 true);
 
-            LOGGER.info("Saved successfully");
+            LOGGER.info("  Passed: rotation={}, mirror={}", rotation, mirror);
           }
-          LOGGER.info("  Passed: rotation={}, mirror={}", rotation, mirror);
-        }
-      }
-    }
+        });
 
     helper.succeed();
   }
