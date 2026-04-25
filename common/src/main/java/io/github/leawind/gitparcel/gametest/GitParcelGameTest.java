ebf4bf34af3e72c750c125162095325523cbbc38
Leawind <leawind@yeah.net>
2026-04-26 00:00:58 +0800

    refactor(gametest): test save and load

--- common/src/main/java/io/github/leawind/gitparcel/gametest/GitParcelGameTest.java
+++ common/src/main/java/io/github/leawind/gitparcel/gametest/GitParcelGameTest.java
@@ -4,31 +4,76 @@ import com.mojang.logging.LogUtils;
 import io.github.leawind.gitparcel.GitParcel;
 import io.github.leawind.gitparcel.api.parcel.ParcelFormatRegistry;
 import io.github.leawind.gitparcel.api.parcel.ParcelStorage;
+import io.github.leawind.gitparcel.gametest.utils.ChannelFlags;
 import io.github.leawind.gitparcel.gametest.utils.GameTestHelpMore;
 import io.github.leawind.gitparcel.gametest.utils.GitParcelTestUtils;
 import io.github.leawind.inventory.misc.TempDirectory;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import org.slf4j.Logger;
 
 public class GitParcelGameTest {
   public static final Logger LOGGER = LogUtils.getLogger();
 
-  public void testSavers(GameTestHelpMore helper) throws Exception {
+  public void testSaveAndLoad(GameTestHelpMore helper) throws Exception {
     GitParcelTestUtils.forEachFormatCombination(
         ParcelFormatRegistry.INSTANCE.streamSavers().toList(),
-        (format, rotation, mirror) -> {
+        (saver, rotation, mirror) -> {
+          LOGGER.info(
+              "Testing format {} with rotation={} mirror={}", saver.spec(), rotation, mirror);
+
           try (var tempDir = new TempDirectory(GitParcel.MOD_ID)) {
-            LOGGER.info(
-                "Testing format {} with rotation={} mirror={}", format.spec(), rotation, mirror);
+            var box = helper.getRelativeBoundingBox();
+            int halfHeight = box.getYSpan() / 2;
+
+            var bottomBox =
+                new BoundingBox(
+                    box.minX(),
+                    box.minY(),
+                    box.minZ(),
+                    box.maxX(),
+                    box.minY() + halfHeight - 1,
+                    box.maxZ());
+
+            var topBox =
+                new BoundingBox(
+                    box.minX(),
+                    box.maxY() + 1 - halfHeight,
+                    box.minZ(),
+                    box.maxX(),
+                    box.maxY(),
+                    box.maxZ());
 
             ParcelStorage.save(
-                format,
+                saver,
                 helper.getLevel(),
-                helper.getBoundingBox(),
+                helper.absoluteBoundingBox(bottomBox),
                 rotation,
                 mirror,
                 null,
                 tempDir.getPath(),
                 true);
+
+            var loader = ParcelFormatRegistry.INSTANCE.getLoader(saver.spec());
+            if (loader == null) {
+              LOGGER.info("  Skipped: no loader for format {}", saver.spec());
+              return;
+            }
+
+            ParcelStorage.load(
+                helper.getLevel(),
+                helper.absoluteBoundingBox(topBox),
+                rotation,
+                mirror,
+                tempDir.getPath(),
+                false,
+                true,
+                Block.UPDATE_CLIENTS
+                    | Block.UPDATE_IMMEDIATE
+                    | Block.UPDATE_KNOWN_SHAPE
+                    | Block.UPDATE_SKIP_ALL_SIDEEFFECTS);
+
+            helper.assertSame(bottomBox, topBox, ChannelFlags.BLOCKS);
 
             LOGGER.info("  Passed: rotation={}, mirror={}", rotation, mirror);
           }
