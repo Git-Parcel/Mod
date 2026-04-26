2d119fe09b356f3b22c2df54e963c7960978f006
Leawind <leawind@yeah.net>
2026-04-26 17:40:26 +0800

    feat(commands): add teleport to parcel command
    /parcel <parcel> teleport [players]

--- common/src/main/java/io/github/leawind/gitparcel/server/commands/parcel/ParcelCommand.java
+++ common/src/main/java/io/github/leawind/gitparcel/server/commands/parcel/ParcelCommand.java
@@ -10,6 +10,7 @@ import io.github.leawind.gitparcel.server.commands.ParcelFormatter;
 import io.github.leawind.gitparcel.server.commands.parcel.config.ConfigSubcommand;
 import io.github.leawind.gitparcel.server.commands.parcel.delete.DeleteSubcommand;
 import io.github.leawind.gitparcel.server.commands.parcel.save.SaveSubcommand;
+import io.github.leawind.gitparcel.server.commands.parcel.tp.TeleportSubcommand;
 import net.minecraft.commands.CommandBuildContext;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
@@ -27,7 +28,8 @@ public class ParcelCommand extends GitParcelBaseCommand {
                     .executes(ParcelCommand::showInfo)
                     .then(ConfigSubcommand.build())
                     .then(DeleteSubcommand.build())
-                    .then(SaveSubcommand.build()));
+                    .then(SaveSubcommand.build())
+                    .then(TeleportSubcommand.build()));
 
     dispatcher.register(parcel);
   }
