8d891ae8ba42dcfcb88e610a28813e0b9892ba7a
Leawind <leawind@yeah.net>
2026-05-08 19:40:17 +0800

    style: remove unused predicate field from ParcelSelectorOptions

--- common/src/main/java/io/github/leawind/gitparcel/commands/arguments/parcelselector/ParcelSelectorOptions.java
+++ common/src/main/java/io/github/leawind/gitparcel/commands/arguments/parcelselector/ParcelSelectorOptions.java
@@ -9,7 +9,6 @@ import com.mojang.logging.LogUtils;
 import io.github.leawind.gitparcel.GitParcelTranslations;
 import java.util.Locale;
 import java.util.Map;
-import java.util.function.Predicate;
 import net.minecraft.commands.arguments.selector.options.EntitySelectorOptions;
 import net.minecraft.network.chat.Component;
 import org.slf4j.Logger;
@@ -24,17 +23,12 @@ public class ParcelSelectorOptions {
   public static final DynamicCommandExceptionType ERROR_UNKNOWN_OPTION =
       new DynamicCommandExceptionType(
           arg -> GitParcelTranslations.of("argument.gitparcel.parcel.options.unknown", arg));
-  public static final DynamicCommandExceptionType ERROR_INAPPLICABLE_OPTION =
-      new DynamicCommandExceptionType(
-          arg -> GitParcelTranslations.esc("argument.gitparcel.parcel.options.inapplicable", arg));
   public static final SimpleCommandExceptionType ERROR_LIMIT_TOO_SMALL =
       new SimpleCommandExceptionType(
           GitParcelTranslations.of("argument.gitparcel.parcel.options.limit.toosmall"));
 
-  // TODO remove predicate field
-  private static void register(
-      String id, Modifier handler, Predicate<ParcelSelectorParser> predicate, Component tooltip) {
-    OPTIONS.put(id, new Option(handler, predicate, tooltip));
+  private static void register(String id, Modifier handler, Component tooltip) {
+    OPTIONS.put(id, new Option(handler, tooltip));
   }
 
   @FunctionalInterface
@@ -55,7 +49,6 @@ public class ParcelSelectorOptions {
           parser.addPredicate(parcel -> name.equals(parcel.meta().name()) != shouldInvertValue);
           parser.setMaxResults(1);
         },
-        parser -> true,
         GitParcelTranslations.of("argument.gitparcel.parcel.options.name.description"));
 
     register(
@@ -69,7 +62,6 @@ public class ParcelSelectorOptions {
           }
           parser.setMaxResults(Math.min(parser.getMaxResults(), limit));
         },
-        parser -> true,
         GitParcelTranslations.of("argument.gitparcel.parcel.options.limit.description"));
   }
 
@@ -77,11 +69,7 @@ public class ParcelSelectorOptions {
       throws CommandSyntaxException {
     var option = OPTIONS.get(id);
     if (option != null) {
-      if (option.canUse.test(parser)) {
-        return option.modifier;
-      } else {
-        throw ERROR_INAPPLICABLE_OPTION.createWithContext(parser.getReader(), id);
-      }
+      return option.modifier;
     } else {
       parser.getReader().setCursor(cursor);
       throw ERROR_UNKNOWN_OPTION.createWithContext(parser.getReader(), id);
@@ -93,8 +81,7 @@ public class ParcelSelectorOptions {
     var usedOptions = parser.getUsedOptions();
 
     for (var entry : OPTIONS.entrySet()) {
-      if (entry.getValue().canUse.test(parser)
-          && !usedOptions.contains(entry.getKey())
+      if (!usedOptions.contains(entry.getKey())
           && entry.getKey().toLowerCase(Locale.ROOT).startsWith(remainingLower)) {
         builder.suggest(
             entry.getKey() + ParcelSelectorParser.SYNTAX_KEY_VALUE_SEPARATOR,
@@ -106,12 +93,12 @@ public class ParcelSelectorOptions {
   public static boolean hasAvailableOptions(ParcelSelectorParser parser) {
     var usedOptions = parser.getUsedOptions();
     for (var entry : OPTIONS.entrySet()) {
-      if (entry.getValue().canUse.test(parser) && !usedOptions.contains(entry.getKey())) {
+      if (!usedOptions.contains(entry.getKey())) {
         return true;
       }
     }
     return false;
   }
 
-  record Option(Modifier modifier, Predicate<ParcelSelectorParser> canUse, Component description) {}
+  record Option(Modifier modifier, Component description) {}
 }
