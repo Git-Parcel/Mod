2ca46c5b6e18dcf66335a132a9ef446a803a2bff
Leawind <leawind@yeah.net>
2026-06-11 13:45:55 +0800

    fix: move lib package-info

--- src/main/java/io/github/leawind/gitparcel/lib/package-info.java
+++ src/main/java/io/github/leawind/gitparcel/lib/package-info.java
@@ -1,5 +1,5 @@
 /// This package contains relocated third-party libraries.
 @ApiStatus.Internal
-package io.github.leawind.gitparcel.common.lib;
+package io.github.leawind.gitparcel.lib;
 
 import org.jetbrains.annotations.ApiStatus;
